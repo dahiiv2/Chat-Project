cda85b99cb51461a3bdb064a3dbe49eea0952122
dahiiv2 <danilnmellera@gmail.com>
2024-12-06 14:28:18 +0100

    Reset next.config.ts to original state

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  experimental: {
-    // This will prevent Next.js from statically generating pages at build time
-    workerThreads: false,
-    cpus: 1
-  }
+  /* config options here */
 };
 
 export default nextConfig;
