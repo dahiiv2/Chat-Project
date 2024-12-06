4192328b8ec194668b43625faaa88ef62a7f9a6a
dahiiv2 <danilnmellera@gmail.com>
2024-12-06 13:36:01 +0100

    test2

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,13 +28,6 @@ export default function RootLayout({
           "bg-white text-slate-900 dark:bg-[#290007] dark:text-slate-50"
 
         )} suppressHydrationWarning>
-          <ThemeProvider 
-          attribute="class" 
-          defaultTheme="dark" 
-          enableSystem={false} 
-          storageKey="theme">
-            {children}
-          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
