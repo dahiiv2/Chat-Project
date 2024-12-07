2d44d361511bf872303825d263b4e7d6e539f337
dahiiv2 <danilnmellera@gmail.com>
2024-12-07 10:47:05 +0100

    error fix i hope maybe perhaps someone help me

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+    // eslint-disable-next-line no-var
     var prisma: PrismaClient | undefined;
 };
 
@@ -8,4 +9,6 @@ export const db = globalThis.prisma || new PrismaClient();
 
 // Store the prisma instance globally in development to prevent exhausting 
 // the database connection limit due to hot reloading
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+if (process.env.NODE_ENV !== "production") {
+    globalThis.prisma = db;
+}
