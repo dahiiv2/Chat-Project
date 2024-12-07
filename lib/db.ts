a1d68534dc7806f813c72e96b164338a77244a9d
dahiiv2 <danilnmellera@gmail.com>
2024-12-07 10:56:15 +0100

    cleared cache PLEASE WORK

--- lib/db.ts
+++ lib/db.ts
@@ -9,6 +9,4 @@ export const db = globalThis.prisma || new PrismaClient();
 
 // Store the prisma instance globally in development to prevent exhausting 
 // the database connection limit due to hot reloading
-if (process.env.NODE_ENV !== "production") {
-    globalThis.prisma = db;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db
