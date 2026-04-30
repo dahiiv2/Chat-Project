a64a3f08b2618e9a9a71941a93cb61042511a0c6
Daniel Mellera <daniel@energygest.com>
2026-04-30 16:30:32 +0200

    Fix Next.js 16 config: migrate to Turbopack and remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    webpack: (config) => {
-        config.externals.push({
-            "utf-8-validate": "commonjs utf-8-validate",
-            "bufferutil": "commonjs bufferutil"
-        })
-        return config;
-    },
+    turbopack: {},
     images: {
-        domains: [
-            "uploadthing.com",
-            "utfs.io"
+        remotePatterns: [
+            { protocol: 'https', hostname: 'uploadthing.com' },
+            { protocol: 'https', hostname: 'utfs.io' },
         ]
     }
 }
